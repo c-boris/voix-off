9486ce65269aff0b9b7a05fb39fbecdda5da7aac
cboris <b.crochet@gmail.com>
2024-09-24 15:23:04 +0200

    up

--- components/CookieConsent.tsx
+++ components/CookieConsent.tsx
@@ -25,7 +25,7 @@ const CookieConsent = () => {
     <div className="fixed bottom-3  mx-2 sm:left-1/2 sm:-translate-x-1/2 text-center text-xs px-5 py-3 z-[999] bg-gray-300 dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75 bg-opacity-75 border border-white border-opacity-40 shadow-2xl rounded-full">
       En utilisant notre site web, vous acceptez notre utilisation des cookies.
       <button
-        className="px-2 mx-2 bg-gray-300 hover:bg-gray-500 border rounded-full items-center dark:text-gray-950"
+        className="px-2 mt-2 mx-2 bg-gray-300 hover:bg-gray-500 border rounded-full items-center dark:text-gray-950 sm:mt-0"
         onClick={giveCookieConsent}
       >
         Accept
