da94f8ed183b36d27528240baabd59c32133de13
cboris <b.crochet@gmail.com>
2024-10-02 08:12:14 +0200

    up

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/microphone.jpg";
+import corpcommentImg from "@/public/cards/card-example-2.jpg";
 
 export const links = [
   {
