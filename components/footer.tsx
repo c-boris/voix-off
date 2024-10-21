6e4fd31da9f8c34a809f855e987ab5471f46b101
cboris <b.crochet@gmail.com>
2024-10-21 15:39:16 +0200

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,25 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className=" px-4 text-center text-gray-500">
-      <small className="pb-10 block ">
-        &copy; {new Date().getFullYear()} Julie Voix Off | Tous droits réservés.
+    <footer className="px-4 text-center text-gray-700 dark:text-white/70">
+      <small className="pb-10 block">
+        &copy; {new Date().getFullYear()}{" "}
+        <a
+          href="https://julievoixoff.com"
+          className="hover:underline transition duration-300"
+        >
+          Julie Voix Off
+        </a>{" "}
+        | Tous droits réservés | Design by{" "}
+        <a
+          href="https://studio-126.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="hover:underline transition duration-300">
+            Studio 126
+          </span>
+        </a>
       </small>
     </footer>
   );
